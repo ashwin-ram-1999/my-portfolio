2a187629112151e56daf298b1f4fc1a1aee0bdd7
Ashwin <ashwinsundarram@gmail.com>
2023-01-19 14:08:25 +0530

    Updated css

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <table>
-        <thead>
-          <th>
-            <tr>
-              <td>LINK 1</td>
-              <td>LINK 2</td>
-              <td>LINK 3</td>
-              <td>LINK 4</td>
-            </tr>
-          </th>
-        </thead>
-      </table>
-      <h1>My Portfolio</h1>
-      <button className="bg-red-500">Click me</button>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="bg-red-500 text-white rounded">Click me</button>
+      <h1 className="font-sans text-5xl m-10">My Portfolio</h1>
+      <div className="text-blue-500 border-2 border-dashed grid grid-cols-4 gap-x-1 place-items-center ">
+        <div>LINK 1</div>
+        <div>LINK 2</div>
+        <div>LINK 3</div>
+        <div>LINK 4</div>
+      </div>
     </div>
   );
 }
