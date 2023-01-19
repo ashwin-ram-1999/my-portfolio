e58881f54a91fa619de9e297cb9581937e52260c
Ashwin <ashwinsundarram@gmail.com>
2023-01-19 13:14:36 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React from "react";
+
 function App() {
   return (
     <div>
+      <table>
+        <thead>
+          <th>
+            <tr>
+              <td>LINK 1</td>
+              <td>LINK 2</td>
+              <td>LINK 3</td>
+              <td>LINK 4</td>
+            </tr>
+          </th>
+        </thead>
+      </table>
       <h1>My Portfolio</h1>
     </div>
   );
